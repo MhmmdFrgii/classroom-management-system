8d5a86005ac87f37723bcad242d0a2ae6a042830
xavier <167852917+ananta079@users.noreply.github.com>
2024-11-05 15:16:19 +0700

    slides

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PagelaranController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SlideController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('pages.dashboard');
