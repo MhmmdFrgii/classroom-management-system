8257773c1ffd95b11e9f615cbbe4dd0718291216
MhmmdFrgii <132626734+MhmmdFrgii@users.noreply.github.com>
2024-11-07 16:03:30 +0700

    Update view pagelaran view and classmeet view

--- resources/views/guest/memory-classmeet.blade.php
+++ resources/views/guest/memory-classmeet.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="w-full px-4">
                 <h1 class="text-7xl font-anton text-slate-200 text-center">
-                    Galery Pagelaran XII RPL
+                    Galery Classmeet XII RPL
                 </h1>
                 <p class="inter-regular text-3xl text-slate-200 text-center">Selamat Menikmati</p>
             </div>
@@ -16,20 +16,20 @@
     <section class="w-full mx-auto min-h-screen flex items-center justify-center text-center text-white bg-black">
         {{-- Galeri Pagelaran --}}
         <div class="mx-auto p-8 grid grid-cols-3 gap-4">
-            @foreach ($pagelarans as $index => $pagelaran)
+            @foreach ($classmeet as $index => $cm)
                 <div
                     class="relative overflow-hidden group
-                    @if ($index == 0) col-span-2 row-span-2 @endif
-                    @if ($index == 1 || $index == 2) col-span-1 row-span-2 @endif
-                    @if ($index == 3) col-span-2 row-span-1 @endif">
+                    @if ($index == 0) col-span-1 row-span-2 @endif
+                    @if ($index == 1 || $index == 2) col-span-2 row-span-2 @endif
+                    @if ($index == 3) col-span-1 row-span-1 @endif">
 
-                    <img src="{{ asset('storage/' . $pagelaran->image) }}" alt="{{ $pagelaran->title }}"
+                    <img src="{{ asset('storage/' . $cm->image) }}" alt="{{ $cm->title }}"
                         class="w-full h-full object-cover transition-transform duration-500 transform group-hover:scale-105">
 
                     <div
                         class="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex flex-col justify-end items-start text-left p-4 pb-4 pl-4">
-                        <h3 class="text-lg font-semibold mb-2">{{ $pagelaran->title }}</h3>
-                        <p class="text-sm inter-light">{{ $pagelaran->description }}</p>
+                        <h3 class="text-lg font-semibold mb-2">{{ $cm->title }}</h3>
+                        <p class="text-sm inter-light">{{ $cm->description }}</p>
                     </div>
                 </div>
             @endforeach
